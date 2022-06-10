08dac666c2d3360676b12db1e1a29b8815979022
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-06-10 13:42:42 +0200

    ADD : coloring

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
+use colored::*;
 // $env:https_proxy="http://proxy29.ad.campus-eni.fr:3128"
 
 fn main() {
+    // RNG using rand lib
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is {}", secret_number);
 
@@ -13,17 +15,26 @@ fn main() {
     loop {
         println!("Input your guess: ");
         let mut guess: String = String::new();
+        // use stdin read_line to capture the user input
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number");
+        // The string needs to be parsed as an int
+        // Except handles the error message
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         println!("You guessed {}", guess);
+        // to compare use std::cmp::Ordering lib
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small"),
-            Ordering::Equal => println!("You win"),
-            Ordering::Greater => println!("Too big"),
+            Ordering::Less => println!("{}", "Too small".red()),
+            Ordering::Greater => println!("{}", "Too big".red()),
+            // break allows to exit the loop
+            Ordering::Equal => {println!("{}", "You win!".green());break;}
+            // Add text coloring using colored lib.
         }
     }
 }
